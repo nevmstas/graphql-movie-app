dad42d6d593360663ea226fde7a83dd752309748
Stas <nevmyvakastas@gmail.com>
2020-10-14 23:26:07 +0600

    wrote basic server

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express =require('express');
+const { graphqlHTTP } = require('express-graphql')
+
+
+const app = express()
+const PORT = 3002
+
+app.use('/graphql', graphqlHTTP({}))
+
+app.listen(PORT, err => {
+    err? console.log(err): console.log('Server started!')
+})
