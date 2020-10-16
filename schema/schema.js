c22c06ea7aec9b8f174fb73dc81fc3a672e5a8f8
Stas <nevmyvakastas@gmail.com>
2020-10-16 23:50:19 +0600

    get all movies, directors query

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,16 @@
 const graphql = require('graphql')
 
-const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLID} = graphql
+const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLID, GraphQLList} = graphql
 
 const movies = [
     {id: '1', name: "Snatch", genre: 'Crime-Comedy', directorId: '4'},
     {id: '2', name: "Pulp Fiction", genre: 'Crime', directorId: '1'},
     {id: '3', name: "V for vendetta", genre: 'Sci-Fi-Triller', directorId: '3'},
-    {id: '4', name: "1984", genre: 'Sci-Fi', directorId: '2'}
+    {id: '4', name: "1984", genre: 'Sci-Fi', directorId: '2'},
+    {id: '5', name: "Reservoir Dogs", genre: 'Crime', directorId: '1'},
+    {id: '6', name: "The Hateful Eight", genre: 'Crime', directorId: '1'},
+    {id: '7', name: "Inglourious Basterds", genre: 'Crime', directorId: '1'},
+    {id: '8', name: "lock, Stock and Two Smoking Barrels", genre: 'Crime-Comedy', directorId: '4'}
 ]
 const directors = [
     {id: '1', name: 'Quentin Tarantino', age: 55},
@@ -36,7 +40,13 @@ const DirectorType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        movies:{
+            type: new GraphQLList(MovieType),
+            resolve(parent, args){
+                return movies.filter(movie => movie.directorId === parent.id)
+            }
+        }
     })
 })
 
@@ -58,6 +68,21 @@ const Query = new GraphQLObjectType({
                return directors.find(director => director.id == args.id) 
             }
         },
+
+        directors :{
+            type: new GraphQLList(DirectorType),
+            resolve(parent, args){
+                return directors
+            }
+        },
+        
+        movies :{
+            type: new GraphQLList(MovieType),
+            resolve(parent, args){
+                return movies
+            }
+        }
+
 
     }  
 })
