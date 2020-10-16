ca6d2576446fe71a02177fd16e285cbc5551eb3d
Stas <nevmyvakastas@gmail.com>
2020-10-16 23:34:12 +0600

    director type

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 const express =require('express');
 const { graphqlHTTP } = require('express-graphql')
+const schema = require('../schema/schema')
 
 
 const app = express()
 const PORT = 3002
 
-app.use('/graphql', graphqlHTTP({}))
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
 
 app.listen(PORT, err => {
     err? console.log(err): console.log('Server started!')
